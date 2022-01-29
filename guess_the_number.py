3e864074516dcab4f20532140a7b0fbe706e3e8c
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2022-01-29 12:04:25 +0200

    another function to reverse the game the user has a number and the computer guesses

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,15 +1,15 @@
 import random 
 
-def guess(x):
+def guess(upper_range):
 	# generate the random number
-	random_number = random.randint(1, x)
+	random_number = random.randint(1, upper_range)
 
 	# init guess
 	guess = 0
 
 	# getting the user's guess
 	while guess != random_number:
-		guess = int(input(f'Guess a number between 1 and {x}: '))
+		guess = int(input(f'Guess a number between 1 and {upper_range}: '))
 		
 		# giving the user some clues
 		if guess > random_number: 
@@ -19,4 +19,29 @@ def guess(x):
 
 	print(f'WOW you got it right, you guessed the right number {random_number}')
 
-guess(10)
+
+def computer_guess():
+	lowest_number = int(input('Enter your lowest number.. '))
+	highest_number = int(input('Enter your highest number.. '))
+	feedback = ''
+
+
+	while feedback != 'c':
+		if lowest_number != highest_number:
+			guessed_number = random.randint(lowest_number, highest_number)
+		else: 
+			guess = lowest_number
+
+		feedback = input(f'Is {guessed_number} correct Enter \
+			your feddback C for correct H for \
+			guess higher and L for guess lower ')
+
+		if feedback == 'H':
+			lowest_number += 1
+		else:
+			highest_number -= 1
+
+	print(f'WOW I guessed your number {guessed_number} correctly')
+
+computer_guess()
+
